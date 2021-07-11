a8acb96d1891e5682e02472dbf5132657ba261f7
Denisse Madeleine <dmadelei@pl-p1.msk.21-school.ru>
2021-07-11 17:16:43 +0300

    push

--- live_philo/live_philo.c
+++ live_philo/live_philo.c
@@ -19,7 +19,6 @@ void *mode_of_life_philo(void *philo_one)
 	 	pthread_mutex_unlock(philo->mutex_philo_said);
 	 	pthread_mutex_unlock(&philo->right_hand->mutex_fork);
 	 	pthread_mutex_unlock(&philo->left_hand->mutex_fork);
-
 	// 	if (philo->count_eat != -1)
 	// 	{
 	// 		philo->count_eat--;
